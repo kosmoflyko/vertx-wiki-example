a43839da389d0caeeb5eaae1e8802d1de987a57c
kosmoflyko <kosmoflyko@gmail.com>
2018-06-15 15:51:37 +0300

    The web client API

--- src/main/java/io/vertx/starter/database/WikiDatabaseService.java
+++ src/main/java/io/vertx/starter/database/WikiDatabaseService.java
@@ -10,6 +10,7 @@ import io.vertx.core.json.JsonObject;
 import io.vertx.ext.jdbc.JDBCClient;
 
 import java.util.HashMap;
+import java.util.List;
 
 @ProxyGen
 public interface WikiDatabaseService {
@@ -36,4 +37,7 @@ public interface WikiDatabaseService {
 
   @Fluent
   WikiDatabaseService deletePage(int id, Handler<AsyncResult<Void>> resultHandler);
+
+  @Fluent
+  WikiDatabaseService fetchAllPageData(Handler<AsyncResult<List<JsonObject>>> resultHandler);
 }
--- src/test/java/io/vertx/starter/SomeTests.java
+++ src/test/java/io/vertx/starter/SomeTests.java
@@ -1,11 +1,15 @@
 package io.vertx.starter;
 
 import io.vertx.core.DeploymentOptions;
+import io.vertx.core.Future;
 import io.vertx.core.Vertx;
+import io.vertx.core.buffer.Buffer;
 import io.vertx.core.json.JsonObject;
 import io.vertx.ext.unit.Async;
 import io.vertx.ext.unit.TestContext;
 import io.vertx.ext.unit.junit.VertxUnitRunner;
+import io.vertx.ext.web.client.HttpResponse;
+import io.vertx.ext.web.client.WebClient;
 import io.vertx.starter.database.WikiDatabaseService;
 import io.vertx.starter.database.WikiDatabaseVerticle;
 import org.junit.After;
@@ -20,7 +24,7 @@ public class SomeTests {
   private WikiDatabaseService service;
 
   @Before
-  public void prepare(TestContext context){
+  public void prepare(TestContext context) {
     vertx = Vertx.vertx();
 
     JsonObject conf = new JsonObject()
@@ -29,7 +33,7 @@ public class SomeTests {
 
     vertx.deployVerticle(new WikiDatabaseVerticle(), new DeploymentOptions().setConfig(conf),
       context.asyncAssertSuccess(id ->
-      service = WikiDatabaseService.createProxy(vertx, WikiDatabaseVerticle.CONFIG_WIKIDB_QUEUE)));
+        service = WikiDatabaseService.createProxy(vertx, WikiDatabaseVerticle.CONFIG_WIKIDB_QUEUE)));
   }
 
   @After
@@ -51,22 +55,22 @@ public class SomeTests {
   public void crud_operations(TestContext context) {
     Async async = context.async();
 
-    service.createPage("Test", "Some content", context.asyncAssertSuccess( v1 -> {
+    service.createPage("Test", "Some content", context.asyncAssertSuccess(v1 -> {
 
       service.fetchPage("Test", context.asyncAssertSuccess(json1 -> {
         context.assertTrue(json1.getBoolean("found"));
         context.assertTrue(json1.containsKey("id"));
         context.assertEquals("Some content", json1.getString("rawContent"));
 
-        service.savePage(json1.getInteger("id"), "Yo!", context.asyncAssertSuccess( v2 -> {
+        service.savePage(json1.getInteger("id"), "Yo!", context.asyncAssertSuccess(v2 -> {
 
-          service.fetchAllPages(context.asyncAssertSuccess( array1 -> {
+          service.fetchAllPages(context.asyncAssertSuccess(array1 -> {
             context.assertEquals(1, array1.size());
 
             service.fetchPage("Test", context.asyncAssertSuccess(json2 -> {
               context.assertEquals("Yo!", json2.getString("rawContent"));
 
-              service.deletePage(json1.getInteger("id"), v3 ->{
+              service.deletePage(json1.getInteger("id"), v3 -> {
 
                 service.fetchAllPages(context.asyncAssertSuccess(array2 -> {
                   context.assertTrue(array2.isEmpty());
@@ -79,5 +83,30 @@ public class SomeTests {
       }));
     }));
     async.awaitSuccess(5000);
+  }
+
+  @Test
+  public void start_http_server(TestContext context) {
+    Async async = context.async();
+
+    vertx.createHttpServer()
+      .requestHandler(req -> req.response().putHeader("Content-Type", "text/plain").end("Ok"))
+      .listen(8080, context.asyncAssertSuccess(server -> {
+
+        WebClient webClient = WebClient.create(vertx);
+
+        webClient.get(8080, "localhost", "/").send(ar -> {
+           if(ar.succeeded()){
+             HttpResponse<Buffer> response = ar.result();
+             context.assertTrue(response.headers().contains("Content-Type"));
+             context.assertEquals("text/plain", response.getHeader("Content-Type"));
+             context.assertEquals("Ok", response.body().toString());
+             webClient.close();
+             async.complete();
+           } else {
+             async.resolve((Future.failedFuture(ar.cause())));
+           }
+        });
+      }));
   }
 }
